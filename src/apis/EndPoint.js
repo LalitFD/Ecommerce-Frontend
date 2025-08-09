0e1372b56aeb260d267bc99162505d69bd3b9abb
Lalit doriya <lalit211035itt@sdbc.ac.in>
2025-08-09 08:00:55 +0530

    initial commit

--- src/apis/EndPoint.js
+++ src/apis/EndPoint.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://ecommerce-beckend.onrender.com"
 
 export default {
     PRODUCT_LIST: BASE_URL + "/get",
